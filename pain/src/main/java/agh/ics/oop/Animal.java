cdbef2e228a22ae62070008982ba84077d642645
Panyloi <panyloi@git.com>
2022-12-23 19:25:24 +0100

    Create constructors for Animal

--- pain/src/main/java/agh/ics/oop/Animal.java
+++ pain/src/main/java/agh/ics/oop/Animal.java
@@ -1,6 +1,42 @@
 package agh.ics.oop;
 
-public class Animal {
+import java.util.ArrayList;
+
+public class Animal implements IMapElement{
+    public IWorldMap map;
+    public int energy;
+    public MapDirection direction;
+    public int startEnergy;
+    public ArrayList<IPositionChangeObserver> observerList = new ArrayList<>();
+    public Genes genes;
+    public Vector2d position;
+
+    // Constructor
+    public Animal(){
+        this.direction = MapDirection.NORTH;
+        this.genes = new Genes();
+        this.position = new Vector2d(1, 1);
+    }
+
+    public Animal(IWorldMap map){
+        this();
+        this.map = map;
+    }
+
+    public Animal(IWorldMap map, Vector2d initialPosition){
+        this(map);
+        this.position = initialPosition;
+    }
+
+    public Animal(IWorldMap map, Vector2d initialPosition, int energy){
+        this(map, initialPosition);
+        this.energy = energy;
+    }
+
+    
+
+
+    @Override
     public Vector2d getPosition() {
         return null;
     }
@@ -29,4 +65,6 @@ public class Animal {
 
     public void move() {
     }
+
+
 }
