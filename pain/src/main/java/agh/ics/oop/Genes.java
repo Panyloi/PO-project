c412fb284afb292e6cfed4079b37ebd0a4ba72e2
Panyloi <panyloi@git.com>
2022-12-23 22:18:58 +0100

    Create Gene class with basic methods

--- pain/src/main/java/agh/ics/oop/Genes.java
+++ pain/src/main/java/agh/ics/oop/Genes.java
@@ -1,2 +1,34 @@
-package agh.ics.oop;public class Genes {
+package agh.ics.oop;
+
+import java.util.Random;
+
+public class Genes {
+    public int[] genes;
+    private int iterator;
+    Random rnd;
+    public Genes(int size){
+        this.genes = new int[size];
+        this.iterator = 0;
+        this.rnd = new Random();
+
+        for(int i = 0; i<size; i++){
+            int genotype = this.randomGene();
+            this.genes[i] = genotype;
+        }
+
+    }
+
+    public int getGene(){
+        return genes[iterator];
+    }
+
+    public int nextGene(){
+        iterator = (iterator + 1) % 8;
+        return genes[iterator];
+    }
+
+    public int randomGene(){
+        return (int) rnd.nextDouble() * 8;
+    }
+
 }
