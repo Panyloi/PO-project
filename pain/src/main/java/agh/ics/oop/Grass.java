971fe95e52e171603b8e753e4208f52f56468f15
Panyloi <panyloi@git.com>
2022-12-28 20:13:32 +0100

    Implemented Grass methods

--- pain/src/main/java/agh/ics/oop/Grass.java
+++ pain/src/main/java/agh/ics/oop/Grass.java
@@ -1,9 +1,22 @@
 package agh.ics.oop;
 
 public class Grass implements IMapElement{
+    private Vector2d position;
+    private static int energy;
     public Grass(Vector2d position) {
+        this.position = position;
+    }
+    public void setEnergy(int energy){
+        Grass.energy = energy;
+    }
+    public int getEnergy(){
+        return Grass.energy;
     }
     public Vector2d getPosition() {
-        return null;
+        return this.position;
+    }
+    @Override
+    public String toString(){
+        return "Grass: " + this.position.toString();
     }
 }
